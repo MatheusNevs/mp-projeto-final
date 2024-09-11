1e92c578667158e028c18aee3ce446c846e8583d
MatheusNevs <matheusnf62@gmail.com>
2024-09-11 12:08:07 -0300

    fix: image test fixed

--- src/test/models/image.test.ts
+++ src/test/models/image.test.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from "@prisma/client/runtime/library";
 
 const db = new PrismaClient();
 
-describe("Images Model", () => {
+describe("Image Model", () => {
   it("Deve criar uma imagem com dados válidos", async () => {
     const createdUser = await db.user.create({
       data: {
@@ -18,57 +21,69 @@ describe("Images Model", () => {
       },
     });
     const validImageData = {
-      objectId: createdDonate.id,
+      donateId: createdDonate.id,
       publicId: "validPublicId",
     };
 
-    const createdImage = await db.images.create({
+    const createdImage = await db.image.create({
       data: { ...validImageData },
     });
 
     expect(createdImage).toHaveProperty("id", createdImage.id);
-    expect(createdImage).toHaveProperty("objectId", validImageData.objectId);
+    expect(createdImage).toHaveProperty("donateId", validImageData.donateId);
     expect(createdImage).toHaveProperty("publicId", validImageData.publicId);
   });
 
-  it("Deve falhar ao criar imagem sem objectId", async () => {
+  it("Deve falhar ao criar imagem sem donateId", async () => {
     const invalidImageData = {
-      objectId: null,
+      donateId: null,
       publicId: "validPublicId",
     };
 
     await expect(
-      db.images.create({
+      db.image.create({
         data: { ...invalidImageData },
       }),
-    ).rejects.toThrow(PrismaClientKnownRequestError);
+    ).rejects.toThrow(PrismaClientValidationError);
   });
 
   it("Deve falhar ao criar imagem sem publicId", async () => {
     const invalidImageData = {
-      objectId: "validObjectId",
+      donateId: "validdonateId",
       publicId: null, // publicId vazio
     };
 
     await expect(
-      db.images.create({
+      db.image.create({
         data: { ...invalidImageData },
       }),
-    ).rejects.toThrow(PrismaClientKnownRequestError);
+    ).rejects.toThrow(PrismaClientValidationError);
   });
 
   it("Deve falhar ao criar imagem com ID duplicado", async () => {
+    const createdUser = await db.user.create({
+      data: {
+        email: "imagetest2@gmail.com",
+      },
+    });
+
+    const createdDonate = await db.donate.create({
+      data: {
+        description: "Doação para teste de imagens",
+        userId: createdUser.id,
+      },
+    });
     const validImageData = {
-      objectId: "validObjectId2",
+      donateId: createdDonate.id,
       publicId: "validPublicId2",
     };
 
-    const createdImage = await db.images.create({
+    const createdImage = await db.image.create({
       data: { ...validImageData },
     });
 
     await expect(
-      db.images.create({
+      db.image.create({
         data: { ...validImageData, id: createdImage.id },
       }),
     ).rejects.toThrow(PrismaClientKnownRequestError);
