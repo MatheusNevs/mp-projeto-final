3e9e2afa4587e2ddddcceb8cea6abb805ab4a973
Sarah <sarah.fernandes@struct.unb.br>
2024-09-11 20:14:53 -0300

    relatorio-sarah

---
+++ .next/server/vendor-chunks/geist@1.3.1_next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1_.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/geist@1.3.1_next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1_";
+exports.ids = ["vendor-chunks/geist@1.3.1_next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1_"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/.pnpm/geist@1.3.1_next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1_/node_modules/geist/dist/sans.js":
+/*!*********************************************************************************************************************************!*\
+  !*** ./node_modules/.pnpm/geist@1.3.1_next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1_/node_modules/geist/dist/sans.js ***!
+  \*********************************************************************************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   GeistSans: () => (/* reexport default export from named module */ next_font_local_target_css_path_node_modules_pnpm_geist_1_3_1_next_14_2_5_react_dom_18_3_1_react_18_3_1_react_18_3_1_node_modules_geist_dist_sans_js_import_arguments_src_fonts_geist_sans_Geist_Variable_woff2_variable_font_geist_sans_weight_100_900_variableName_GeistSans___WEBPACK_IMPORTED_MODULE_0__)\n/* harmony export */ });\n/* harmony import */ var next_font_local_target_css_path_node_modules_pnpm_geist_1_3_1_next_14_2_5_react_dom_18_3_1_react_18_3_1_react_18_3_1_node_modules_geist_dist_sans_js_import_arguments_src_fonts_geist_sans_Geist_Variable_woff2_variable_font_geist_sans_weight_100_900_variableName_GeistSans___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! next/font/local/target.css?{\"path\":\"node_modules/.pnpm/geist@1.3.1_next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1_/node_modules/geist/dist/sans.js\",\"import\":\"\",\"arguments\":[{\"src\":\"./fonts/geist-sans/Geist-Variable.woff2\",\"variable\":\"--font-geist-sans\",\"weight\":\"100 900\"}],\"variableName\":\"GeistSans\"} */ \"(rsc)/./node_modules/.pnpm/next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/next/font/local/target.css?{\\\"path\\\":\\\"node_modules/.pnpm/geist@1.3.1_next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1_/node_modules/geist/dist/sans.js\\\",\\\"import\\\":\\\"\\\",\\\"arguments\\\":[{\\\"src\\\":\\\"./fonts/geist-sans/Geist-Variable.woff2\\\",\\\"variable\\\":\\\"--font-geist-sans\\\",\\\"weight\\\":\\\"100 900\\\"}],\\\"variableName\\\":\\\"GeistSans\\\"}\");\n\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvLnBucG0vZ2Vpc3RAMS4zLjFfbmV4dEAxNC4yLjVfcmVhY3QtZG9tQDE4LjMuMV9yZWFjdEAxOC4zLjFfX3JlYWN0QDE4LjMuMV8vbm9kZV9tb2R1bGVzL2dlaXN0L2Rpc3Qvc2Fucy5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUVhQTtBQUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL21wLXByb2pldG8tZmluYWwvLi9ub2RlX21vZHVsZXMvLnBucG0vZ2Vpc3RAMS4zLjFfbmV4dEAxNC4yLjVfcmVhY3QtZG9tQDE4LjMuMV9yZWFjdEAxOC4zLjFfX3JlYWN0QDE4LjMuMV8vbm9kZV9tb2R1bGVzL2dlaXN0L2Rpc3Qvc2Fucy5qcz8zNjVlIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBsb2NhbEZvbnQgZnJvbSBcIm5leHQvZm9udC9sb2NhbFwiO1xuXG5leHBvcnQgY29uc3QgR2Vpc3RTYW5zID0gbG9jYWxGb250KHtcbiAgc3JjOiBcIi4vZm9udHMvZ2Vpc3Qtc2Fucy9HZWlzdC1WYXJpYWJsZS53b2ZmMlwiLFxuICB2YXJpYWJsZTogXCItLWZvbnQtZ2Vpc3Qtc2Fuc1wiLFxuICB3ZWlnaHQ6IFwiMTAwIDkwMFwiLFxufSk7XG4iXSwibmFtZXMiOlsiR2Vpc3RTYW5zIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/.pnpm/geist@1.3.1_next@14.2.5_react-dom@18.3.1_react@18.3.1__react@18.3.1_/node_modules/geist/dist/sans.js\n");
+
+/***/ })
+
+};
+;
