3c9dcedabe456eba3d40d762c417557ef164fad2
paulo <paulofvilarimda@gmail.com>
2024-09-11 17:11:18 -0300

    some fixes

--- src/app/minhas-doacoes/page.tsx
+++ src/app/minhas-doacoes/page.tsx
@@ -15,7 +15,7 @@ export default async function MinhasDacoes() {
           width={1000}
           height={1000}
         />
-        <h1 className="font-irish-grover text-4xl text-white drop-shadow-[0_0_3px_rgba(0,0,0,1)]">
+        <h1 className="-mt-40 rounded bg-white bg-opacity-40 p-2 font-irish-grover text-4xl text-white drop-shadow-[0_0_3px_rgba(0,0,0,1)]">
           Minhas Doações
         </h1>
         <ul className="no-scrollbar mb-5 flex h-full w-screen flex-col items-center overflow-y-scroll">
